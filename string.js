2bef49a5acf78323e66f5ec169df561bde2921f2
Soft-whiz <softwhiz12@gmail.com>
2024-12-09 21:57:51 +0100

    Create string.js

---
+++ string.js
@@ -0,0 +1,13 @@
+function string(){
+let string = "123456"
+let array = string.split('')
+
+if (string>0){
+    console.log(array)
+}
+else{
+console.log("no input!")
+}
+return array
+}
+return(string ())
